79a21f515fae438b531bda919f82780d6b85fd66
Emma Lanctot <emmalanctot@gmail.com>
2017-05-01 08:44:30 -0400

    users controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,3 +1,19 @@
 class RoomsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
+
+  def index
+    @rooms = Rooms.all
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :user_id, :created_at)
+  end
 
 end
