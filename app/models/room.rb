22edbdceb18b936df05e27a2ee38a0aa70c8b57f
Emma Lanctot <emmalanctot@gmail.com>
2017-05-03 20:39:10 -0400

    made rake tasks for plant health decline

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,9 +4,9 @@ class Room < ActiveRecord::Base
   validates :name, presence: true
   validates :user_id, presence: true
   validates_uniqueness_of :user_id
-  validates :plant_health, :numericality => { :greater_than => 0, :less_than_or_equal_to => 10 }
+  validates :plant_health, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10 }
 
-  def plant_decline
+  def decline
     if plant_health.positive?
       self.plant_health = self.plant_health - 1
     else
@@ -15,9 +15,9 @@ class Room < ActiveRecord::Base
   end
 
   def room_decline
-    plant_health.decay
-    plant_health.save
-    return (plant_health)
+    self.decline
+    self.save
+    return (self.plant_health)
   end
 
 end
